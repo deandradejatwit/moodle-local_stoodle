455f5ef5f458b9747f1ed59158c173f024f46568
sullivanm22 <sullivanm22@wit.edu>
2024-06-26 15:08:15 -0400

    Update database
    version 2024062402

--- db/upgrade.php
+++ db/upgrade.php
@@ -36,65 +36,36 @@ defined('MOODLE_INTERNAL') || die();
 function xmldb_local_stoodle_upgrade($oldversion): bool {
     global $CFG, $DB;
     $dbman = $DB->get_manager();
-    if ($oldversion < 2024061603) {
-        // Define table local_stoodle to be created.
-        $table = new xmldb_table('local_stoodle');
-        // Adding fields to table local_stoodle.
-        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
-        // Adding keys to table local_stoodle.
-        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
-        // Conditionally launch create table for local_stoodle.
-        if (!$dbman->table_exists($table)) {
-            $dbman->create_table($table);
-        }
-        // Define table flashcard_test to be created.
+    if ($oldversion < 2024062402) {
+        // Define table flashcard_card to be dropped.
         $table = new xmldb_table('flashcard_test');
-        // Adding fields to table flashcard_test.
-        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
-        $table->add_field('flashcard_question', XMLDB_TYPE_TEXT, null, null, XMLDB_NOTNULL, null, null);
-        $table->add_field('flashcard_answer', XMLDB_TYPE_CHAR, '258', null, XMLDB_NOTNULL, null, null);
-        $table->add_field('test', XMLDB_TYPE_INTEGER, '1', null, null, null, null);
-        // Adding keys to table flashcard_test.
-        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
-        // Conditionally launch create table for flashcard_test.
-        if (!$dbman->table_exists($table)) {
-            $dbman->create_table($table);
+
+        // Conditionally launch drop table for flashcard_card.
+        if ($dbman->table_exists($table)) {
+            $dbman->drop_table($table);
         }
-        // Define table flashcard_set to be created.
+        // Define field usermodified to be added to flashcard_set.
         $table = new xmldb_table('flashcard_set');
+        $field = new xmldb_field('usermodified', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0', 'timemodified');
 
-        // Adding fields to table flashcard_set.
-        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
-        $table->add_field('set_name', XMLDB_TYPE_TEXT, null, null, XMLDB_NOTNULL, null, null);
-        $table->add_field('timemodified', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
-
-        // Adding keys to table flashcard_set.
-        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
-
-        // Conditionally launch create table for flashcard_set.
-        if (!$dbman->table_exists($table)) {
-            $dbman->create_table($table);
+        // Conditionally launch add field usermodified.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
         }
-        // Define table flashcard_card to be created.
-        $table = new xmldb_table('flashcard_card');
+         // Define key usermodified (foreign) to be added to flashcard_set.
+         $table = new xmldb_table('flashcard_set');
+         $key = new xmldb_key('usermodified', XMLDB_KEY_FOREIGN, ['usermodified'], 'user', ['id']);
 
-        // Adding fields to table flashcard_card.
-        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
-        $table->add_field('flashcard_set', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
-        $table->add_field('question', XMLDB_TYPE_TEXT, null, null, XMLDB_NOTNULL, null, null);
-        $table->add_field('answer', XMLDB_TYPE_TEXT, null, null, XMLDB_NOTNULL, null, null);
-        $table->add_field('timemodified', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0');
+         // Launch add key usermodified.
+         $dbman->add_key($table, $key);
+          // Changing type of field set_name on table flashcard_set to char.
+        $table = new xmldb_table('flashcard_set');
+        $field = new xmldb_field('set_name', XMLDB_TYPE_CHAR, '128', null, XMLDB_NOTNULL, null, null, 'id');
 
-        // Adding keys to table flashcard_card.
-        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
-        $table->add_key('foreign', XMLDB_KEY_FOREIGN, ['flashcard_set'], 'flashcard_set', ['id']);
-
-        // Conditionally launch create table for flashcard_card.
-        if (!$dbman->table_exists($table)) {
-            $dbman->create_table($table);
-        }
+        // Launch change of type for field set_name.
+        $dbman->change_field_type($table, $field);
         // Stoodle savepoint reached.
-        upgrade_plugin_savepoint(true, 2024061603, 'local', 'stoodle');
+        upgrade_plugin_savepoint(true, 2024062402, 'local', 'stoodle');
     }
     return true;
 }
