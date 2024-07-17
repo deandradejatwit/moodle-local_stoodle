571169a583c9c2ea2707ece38cd11de26728c77c
kongshijatwit <kongshij@wit.edu>
2024-07-17 10:19:52 -0400

    attempting to display questions

--- amd/src/quiz.js
+++ amd/src/quiz.js
@@ -1,10 +1,68 @@
 export const init = () => {
-    window.console.log("something");
-    const questionSet = document.getElementById("question-set").innerHTML;
-    const answerSet = document.getElementById("answer-set").innerHTML;
-    window.console.log(JSON.parse(questionSet));
-    window.console.log(JSON.parse(answerSet));
+    const questionDiv = document.querySelector(".js-answer-area");
+    const questionSet = JSON.parse(document.getElementById("question-set").innerHTML);
+    const answerSet = JSON.parse(document.getElementById("answer-set").innerHTML);
+    // window.console.log(questionSet);
+    // window.console.log(answerSet);
+
+    for (const key in Object.values(questionSet)) {
+        // Check if it's a multiple choice question
+        if (Object.values(questionSet)[key].is_multiple_choice === 0) {
+            window.console.log("Not multiple choice");
+            continue;
+        }
+
+        // Create a new div to house the question-answer pair
+        const newDiv = questionDiv.appendChild(document.createElement("div"));
+        newDiv.appendChild(document.createElement("p"))
+            .textContent = "Question " + (parseInt(key) + 1) + ": " + Object.values(questionSet)[key].question_text;
+        createMultipleChoiceQuestion(newDiv, Object.values(questionSet)[key].id, answerSet);
+    }
+
+    /**
+     * Creates a multiple choice question.
+     *
+     * @param {object} parent
+     * @param {int} questionId
+     * @param {object} answerSet
+     */
+    function createMultipleChoiceQuestion(parent, questionId, answerSet) {
+        for (const key in Object.values(answerSet)) {
+            // Match answer id to question id
+            if (Object.values(answerSet)[key].question_id === questionId) {
+                createInputNode(parent, "someid", ("someName" + questionId), Object.values(answerSet)[key].option_text);
+            }
+        }
+    }
+
+    /**
+     * Creates a multiple choice question.
+     *
+     * @param {object} parent
+     * @param {string} id
+     * @param {string} name
+     * @param {string} value
+     */
+    function createInputNode(parent, id, name, value) {
+        // Create a radio input
+        const radio = document.createElement("input");
+        radio.type = "radio";
+        radio.id = id;
+        radio.name = name;
+        radio.value = value;
+
+        // Create the label to go with it
+        const label = document.createElement("label");
+        label.appendChild(document.createTextNode(value));
+
+        // Append them to the parent
+        parent.appendChild(radio);
+        parent.appendChild(label);
+        parent.appendChild(document.createElement("br"));
+    }
 };
+
+
 
 export const other = () => {
     // fetch("quiz_activity.php").then(res => res.json()).then(data => {
