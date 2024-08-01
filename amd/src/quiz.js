9101b43615120c27c2698db602d7d949f8d7f29f
kongshijatwit <kongshij@wit.edu>
2024-08-01 17:59:18 -0400

    Create map for question/answer

--- amd/src/quiz.js
+++ amd/src/quiz.js
@@ -5,6 +5,14 @@ export const init = () => {
     const scoreArea = document.querySelector(".score");
     const totalQuestions = Object.values(questionSet).length;
 
+    const newSet = createFullSet(questionSet, answerSet);
+    const typeSet = declareQuestionTypes(questionSet, answerSet);
+    window.console.log(questionSet);
+    window.console.log(answerSet);
+    window.console.log([...newSet.entries()]);
+    window.console.log(typeSet);
+    window.console.log(newSet.get("q1")[0][0]);
+
     scoreArea.innerHTML = "Score: 0 / " + totalQuestions;
     document.querySelector(".submit-button").addEventListener('click', () => {
         questionValidation();
@@ -29,6 +37,8 @@ export const init = () => {
         }
         questionDiv.appendChild(newDiv);
     }
+
+
 
     /**
      * Creates a multiple choice question.
@@ -94,6 +104,25 @@ export const init = () => {
      */
     function questionValidation() {
         let numCorrect = 0;
+        // const questionTypes = declareQuestionTypes(questionSet, answerSet);
+
+        // for (const key in Object.values(questionSet)) {
+        //     const questionText = "Question " + (parseInt(key) + 1) + ": " + Object.values(questionSet)[key].question_text;
+        //     const question = Object.values(questionSet)[key].id;
+        //     let option = null;
+
+        //     if (questionTypes[key] === 0) {
+        //         option = document.getElementById("option_" + question);
+        //     } else if (questionTypes[key] === 1) {
+        //         option = document.querySelector('input[name = "' + question + '"]:checked');
+        //         if (checkMultipleChoice(answerSet, question, option)) {
+        //             window.console.log("Question " + (parseInt(key) + 1) + " is correct");
+        //             numCorrect++;
+        //             parent.children[0].innerText = questionText + " \u{2705}";
+        //         }
+        //     }
+        // }
+
         for (const key in Object.values(questionSet)) {
             const question = Object.values(questionSet)[key].id;
             const parent = document.getElementById("option_" + question).parentElement.parentElement;
@@ -131,5 +160,82 @@ export const init = () => {
         }
 
         scoreArea.innerText = "Score: " + numCorrect + " / " + totalQuestions;
+    }
+
+    /**
+     * Validates Multiple Choice questions.
+     *
+     * @param {Object} options
+     * @param {integer} questionId
+     * @param {Object} selectedOption
+     * @return {boolean} True or false depending on if answer is correct.
+     */
+    function checkMultipleChoice(options, questionId, selectedOption) {
+        for (const key in Object.values(options)) {
+            if (Object.values(options)[key].stoodle_quiz_questionsid === questionId) {
+                if (Object.values(options)[key].option_text === selectedOption.value && Object.values(options)[key].is_correct) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Returns an array with question types.
+     * 0 - Open Response,
+     * 1 - Multiple Choice,
+     * 2 - Select All
+     *
+     * @param {Object} questions
+     * @param {Object} options
+     * @return {Array} Resulting array with question types.
+     */
+    function declareQuestionTypes(questions, options) {
+        const questionTypesArray = new Array(Object.values(questions).length);
+        for (const questionKey in Object.values(questions)) {
+            const questionId = parseInt(Object.values(questions)[questionKey].id);
+            let count = 0;
+            for (const answerKey in Object.values(options)) {
+                const answerObject = Object.values(options)[answerKey];
+                if (parseInt(answerObject.stoodle_quiz_questionsid) === questionId && parseInt(answerObject.is_correct) === 1) {
+                    count++;
+                }
+            }
+            if (count > 1) {
+                questionTypesArray[questionKey] = 2;
+            } else if (count === 1 && parseInt(Object.values(questions)[questionKey].is_multiple_choice) === 1) {
+                questionTypesArray[questionKey] = 1;
+            } else {
+                questionTypesArray[questionKey] = 0;
+            }
+        }
+        return questionTypesArray;
+    }
+
+
+    /**
+     * Return an array of questions and answers.
+     *
+     * @param {Object} qSet
+     * @param {Object} oSet
+     * @return {Map} Resulting array
+     */
+    function createFullSet(qSet, oSet) {
+        let resultSet = new Map();
+        for (const qKey in Object.values(qSet)) {
+            let optionArray = [];
+            let answerArray = [];
+            for (const oKey in Object.values(oSet)) {
+                if (Object.values(qSet)[qKey].question_number === Object.values(oSet)[oKey].stoodle_quiz_questionsid) {
+                    optionArray.push(Object.values(oSet)[oKey].option_text);
+                    if (parseInt(Object.values(oSet)[oKey].is_correct) === 1) {
+                        answerArray.push(Object.values(oSet)[oKey].option_text);
+                    }
+                }
+            }
+            resultSet.set(Object.values(qSet)[qKey].question_text, [optionArray, answerArray]);
+        }
+        return resultSet;
     }
 };
