e2a5b23b53cc62c092c408f05cc0d8df6e7ca63c
kongshij <71053495+kongshijatwit@users.noreply.github.com>
2024-10-15 00:58:26 -0400

    flashcard html fix
    put html from flashcard.php into separate template file

--- flashcard.php
+++ flashcard.php
@@ -83,43 +83,11 @@ if ($select->no_submit_button_pressed()) {
 }
 
 echo $OUTPUT->header();
+
 $select->display();
-?>
 
-<html lang="en">
+$templatecontext = (object)[];
 
-<body>
-    <div class="nav-buttons">
-        <a href="flashcard_create.php"><button class="btn btn-primary">Create New Set</button></a>
-        <a href="index.php"><button class="btn btn-secondary">Back</button></a>
-    </div>
-</body>
+echo $OUTPUT->render_from_template('local_stoodle/flashcard', $templatecontext);
 
-</html>
-
-<?php
-// This file is part of Moodle - https://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
-
-/**
- *
- *
- * @package     local_stoodle
- * @copyright   2024 Jonathan Kong-Shi kongshij@wit.edu,
- *              Myles R. Sullivan sullivanm22@wit.edu,
- *              Jhonathan Deandrade deandradej@wit.edu
- * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
 echo $OUTPUT->footer();
